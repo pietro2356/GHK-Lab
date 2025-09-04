4c3cbbba18cedb8579c22ba717b5be4d5c825f43
Pietro Rocchio <pietro.rocchio@gmail.com>
2025-09-04 23:33:32 +0200

    feat: update PERMEABILITY_MAX_VALUE constant to 999

--- src/app/core/constant/constant.ts
+++ src/app/core/constant/constant.ts
@@ -1,9 +1,8 @@
 export const PERMEABILITY_MIN_VALUE = 1;
-export const PERMEABILITY_MAX_VALUE = 1000;
+export const PERMEABILITY_MAX_VALUE = 999;
 
 export const CONC_OUT_MIN_VALUE = 1;
 export const CONC_OUT_MAX_VALUE = 600;
 
 export const CONC_IN_MIN_VALUE = 1;
 export const CONC_IN_MAX_VALUE = 200;
-
