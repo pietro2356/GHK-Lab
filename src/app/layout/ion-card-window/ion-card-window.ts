0f18d159deeff01ee0978ac37eb5ac79b7ee9640
Pietro Rocchio <pietro.rocchio@gmail.com>
2025-09-05 17:58:57 +0200

    feat: implement dynamic ion results display and improve ion management in GHK lab interface

--- src/app/layout/ion-card-window/ion-card-window.ts
+++ src/app/layout/ion-card-window/ion-card-window.ts
@@ -1,12 +1,16 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { Button } from 'primeng/button';
+import { FormsModule } from '@angular/forms';
+import { IonCard } from '@ui/ion-card/ion-card';
+import { GHKervice } from '@core/services/GHK/ghk/ghk';
 
 @Component({
   selector: 'ghk-ion-card-window',
-  imports: [],
+  imports: [Button, FormsModule, IonCard],
   templateUrl: './ion-card-window.html',
   styleUrl: './ion-card-window.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class IonCardWindow {
-
+  ghkSrv = inject(GHKervice);
 }
--- src/app/layout/results/results.ts
+++ src/app/layout/results/results.ts
@@ -1,12 +1,13 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { GHKervice } from '@core/services/GHK/ghk/ghk';
 
 @Component({
   selector: 'ghk-results',
   imports: [],
   templateUrl: './results.html',
   styleUrl: './results.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Results {
-
+  ghkSrv = inject(GHKervice);
 }
